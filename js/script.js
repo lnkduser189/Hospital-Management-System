3fd33787a2aab01f283ce28edc682e1df04bee34
Vaishnavi Rushikesh Matade <vaishnavi@Vaishnavis-MacBook-Air.local>
2026-08-02 21:35:10 +0530

    Added local storage appointment management

--- js/script.js
+++ js/script.js
@@ -8,7 +8,10 @@ function scrollToAppointment(){
 
 
 
+
+
 let form = document.getElementById("appointmentForm");
+
 
 
 form.addEventListener("submit", function(event){
@@ -17,9 +20,138 @@ form.addEventListener("submit", function(event){
     event.preventDefault();
 
 
+
+    let appointment = {
+
+
+        name:
+        document.getElementById("name").value,
+
+
+        email:
+        document.getElementById("email").value,
+
+
+        doctor:
+        document.getElementById("doctor").value,
+
+
+        date:
+        document.getElementById("date").value,
+
+
+        time:
+        document.getElementById("time").value
+
+
+    };
+
+
+
+    // Get old appointments
+
+    let appointments =
+    JSON.parse(localStorage.getItem("appointments")) || [];
+
+
+
+    // Add new appointment
+
+    appointments.push(appointment);
+
+
+
+    // Save data
+
+    localStorage.setItem(
+        "appointments",
+        JSON.stringify(appointments)
+    );
+
+
+
     alert("Appointment Booked Successfully!");
+
+
 
     form.reset();
 
 
+
+    displayAppointments();
+
+
 });
+
+
+
+
+
+
+function displayAppointments(){
+
+
+    let appointmentList =
+    document.getElementById("appointmentList");
+
+
+
+    appointmentList.innerHTML="";
+
+
+
+    let appointments =
+    JSON.parse(localStorage.getItem("appointments")) || [];
+
+
+
+    appointments.forEach(function(app){
+
+
+
+        let card =
+        document.createElement("div");
+
+
+
+        card.className="appointment-card";
+
+
+
+        card.innerHTML=`
+
+        <h3>${app.name}</h3>
+
+        <p>
+        Doctor: ${app.doctor}
+        </p>
+
+
+        <p>
+        Date: ${app.date}
+        </p>
+
+
+        <p>
+        Time: ${app.time}
+        </p>
+
+        `;
+
+
+
+        appointmentList.appendChild(card);
+
+
+
+    });
+
+
+
+}
+
+
+
+// Display saved appointments when page loads
+
+displayAppointments();
